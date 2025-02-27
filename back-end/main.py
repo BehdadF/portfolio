dab7506e085bc1d2b507b7848e0a811e26868bd1
BehdadF <152574924+BehdadF@users.noreply.github.com>
2025-02-26 18:36:21 -0800

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -18,10 +18,11 @@ async def root():
 
 @app.get("/{name}")
 async def get_cheatsheet(name: str):
-    cheatsheet = await collection.find_one({"_id": name})
+    return {f"{name}"}
+    # cheatsheet = await collection.find_one({"_id": name})
 
-    if cheatsheet:
-        cheatsheet["_id"] = str(cheatsheet["_id"])  # Convert ObjectId to string
-        return cheatsheet
-    else:
-        raise HTTPException(status_code=404, detail="Cheatsheet not found")
+    # if cheatsheet:
+    #     cheatsheet["_id"] = str(cheatsheet["_id"])  # Convert ObjectId to string
+    #     return cheatsheet
+    # else:
+    #     raise HTTPException(status_code=404, detail="Cheatsheet not found")
