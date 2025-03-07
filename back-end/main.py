13ab81570917c5cdc493eb3cb0c13c5e230d52d6
BehdadF <152574924+BehdadF@users.noreply.github.com>
2025-03-07 02:45:03 -0800

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://10.0.57.10:5173", "https://www.behdadf.com"],
+    allow_origins=["https://10.0.57.10:5173", "https://www.behdadf.com", "0.0.0.0"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Authorization"],
@@ -33,21 +33,26 @@ print(collection.find())
 @app.get("/")
 async def root():
     result = collection.find()
+    if result:    
+        for doc in result:
+            ids = doc['_id']
+         
+            print("id" ,ids)
     # return {"You need to specify an endpoint"}
-    return result
+    return ids
 
-@app.get("/ids")
-async def get_ids():
-    documents = await collection.find({}, {"_id": 1}).to_list(length=None)
-    ids = [str(doc["_id"]) for doc in documents]
-    return {"ids": ids}
+# @app.get("/ids")
+# async def get_ids():
+#     documents = await collection.find({}, {"_id": 1}).to_list(length=None)
+#     ids = [str(doc["_id"]) for doc in documents]
+#     return {"ids": ids}
 
-@app.get("/{name}")
-async def get_cheatsheet(name: str):
-    cheatsheet = await collection.find_one({"_id": name})
+# @app.get("/{name}")
+# async def get_cheatsheet(name: str):
+#     cheatsheet = await collection.find_one({"_id": name})
 
-    if cheatsheet:
-        cheatsheet["_id"] = str(cheatsheet["_id"])
-        return cheatsheet
-    else:
-        raise HTTPException(status_code=404, detail="Cheatsheet not found")
+#     if cheatsheet:
+#         cheatsheet["_id"] = str(cheatsheet["_id"])
+#         return cheatsheet
+#     else:
+#         raise HTTPException(status_code=404, detail="Cheatsheet not found")
