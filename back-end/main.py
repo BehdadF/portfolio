f365b84b61e2d07229623da838e01d7e672d9a84
BehdadF <152574924+BehdadF@users.noreply.github.com>
2025-03-25 22:00:21 -0700

    Update main.py

--- back-end/main.py
+++ back-end/main.py
@@ -37,22 +37,11 @@ async def root():
     return {"message": "You need to specify an endpoint"}
 
 @app.get("/ids")
-async def get_ids():
-    # documents = await collection.find({}, {"_id": 1}).to_list(length=None)
-    # ids = [str(doc["_id"]) for doc in documents]
-    # return {"ids": ids}
-    try:
-        object_id = ObjectId('ids')
-    except:
-        raise HTTPException(status_code=400, detail="Invalid ID format")
+ async def get_ids():
+     documents = await collection.find({}, {"_id": 1}).to_list(length=None)
+     ids = [str(doc["_id"]) for doc in documents]
+     return {"ids": ids}
 
-    cheatsheetNames = await collection.find_one({"_id": object_id})
-
-    if cheatsheetNames is None:
-        raise HTTPException(status_code=404, detail="Cheatsheet not found")
-
-    cheatsheetNames["_id"] = str(cheatsheetNames["_id"])  # Convert ObjectId to string
-    return cheatsheetNames
 @app.get("/{name}")
 async def get_cheatsheet(name: str):
     try:
