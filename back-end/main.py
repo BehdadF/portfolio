e323ebec20d25e3734f51c1e3fa9b75a6227a0d2
BehdadF <152574924+BehdadF@users.noreply.github.com>
2025-03-25 20:01:10 -0700

    use regex to include all subdomains

--- back-end/main.py
+++ back-end/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.behdadf.com", "https://behdadf.com"], 
+    allow_origin_regex=r"https://(?:.*\.)?behdadf\.com",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["Content-Type", "Authorization"],
